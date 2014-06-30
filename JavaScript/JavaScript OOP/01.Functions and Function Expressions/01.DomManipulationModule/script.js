5615d9dc845ebe6b73ad73f2cfbc37609f4406dc
Shoilev <martin.shoilev@gmail.com>
2014-06-30 16:10:58 +0300

    Javascript Function and Classical OOP

---
+++ JavaScript/JavaScript OOP/01.Functions and Function Expressions/01.DomManipulationModule/script.js
@@ -0,0 +1,51 @@
+﻿var domModule = (function () {
+    function appendElement(element, parentSelector) {
+        var parent = document.querySelectorAll(parentSelector);
+        for (var i = 0; i < parent.length; i++) {
+            parent[i].appendChild(element.cloneNode(true));
+        }
+    }
+
+    function removeElement(selector) {
+        var element = document.querySelectorAll(selector);
+        if (element.length <= 0) {
+            console.log('No more divs for remove');
+        }
+        else {
+            element[element.length - 1].parentElement.removeChild(element[element.length - 1]);
+        }
+       
+    }
+
+    function addEvent(selector, eventType, eventHandler) {
+        var elements = document.querySelectorAll(selector);
+        for (var i = 0; i < elements.length; i++) {
+            elements[i].addEventListener(eventType, eventHandler);
+        }
+
+    }
+
+    var elementsBuffer = [];
+    var MAX_BUFFER_SIZE = 10;
+    function appendToBuffer(element, parentSelector) {
+        elementsBuffer.push(element);
+        var parent = document.querySelector(parentSelector);
+
+        if (elementsBuffer.length === MAX_BUFFER_SIZE) {
+            var fragment = document.createDocumentFragment();
+            for (var i = 0; i < MAX_BUFFER_SIZE; i++) {
+                fragment.appendChild(elementsBuffer[i]);
+            }
+            parent.appendChild(fragment);
+            elementsBuffer = [];
+        }
+    }
+
+
+    return {
+        appendElement: appendElement,
+        removeElement: removeElement,
+        addEvent: addEvent,
+        appendToBuffer: appendToBuffer
+    }
+}());
---
+++ JavaScript/JavaScript OOP/01.Functions and Function Expressions/03.ConsoleModule/script.js
@@ -0,0 +1,30 @@
+﻿var specialConsole = (function () {
+
+    function formatString(args) {
+        var text = args[0];
+        if (args.length > 1) {     
+            for (i = 0; i < args.length-1; i++) {
+                 text =  text.replace('{' + (i) + '}', args[i + 1]);
+            }
+        }
+         return text;
+    }
+
+    function writeLine() {
+        console.log(formatString(arguments));
+    }
+
+    function writeError() {
+        console.error(formatString(arguments));
+    }
+
+    function writeWarn() {
+        console.warn(formatString(arguments));
+    }
+
+    return {
+        writeLine: writeLine,
+        writeError: writeError,
+        writeWarn: writeWarn
+    }
+})();
